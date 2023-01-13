24ce0aeaabd69e1d5568a237546bdd627a308744
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-01-13 16:21:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function palindrome(str){
 const len = str.length;
 	for (let i=0; i<len/2; i++){
-		if(str[i] !== str[len -1 -i]){
+		if(str[i] != str[len -1 -i]){
 			return false;
 			break
 		}else{
