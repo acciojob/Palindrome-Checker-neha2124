6532f32f5a35aecdac8af81e9b5fd62075aedafa
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-01-13 17:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
 
 function palindrome(str){
 	let strb = str.toLowerCase()
-const len = strb.length;
-	
-	for (let i=0; i<len/2; i++){
-		if(strb[i] !== strb[len -i-1]){
-			return false;
-		
-		}else{
-			return true;
-		}
+const len = strb.length-1;
+	  // let j = str.length -1;
+      for( let i = 0 ; i < len/2 ;i++)
+      {
+        let x = str[i] ;//forward character
+        let y = str[len-i];//backward character
+        if( x != y)
+        {
+          // return false if string not match
+          return false;
+        }
+      }
+      /// return true if string is palindrome
+      return true;
+
 	}
 	// console.log(str)
 	
