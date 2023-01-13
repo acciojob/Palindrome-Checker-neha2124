7597ee5724456af4ac108dd5485c3b967ff2c7d5
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-01-13 16:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // complete the given function
 
 function palindrome(str){
-
+const len = str.length;
+	for (let i=0; i<len/2; i++){
+		if(str[i] != str[i-1]){
+			return false;
+			break
+		}else{
+			return true;
+		}
+	}
+	// console.log(str)
+	
 }
 module.exports = palindrome
