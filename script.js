c049e0d91df1be84d38a8eee92032220e693cb4d
neha2124 <68601648+neha2124@users.noreply.github.com>
2023-01-13 17:37:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 function palindrome(str){
 	let strb = str.toLowerCase()
 const len = strb.length;
+	if(len == 1){
+		return true
+	}
 	
 	for (let i=0; i<len/2; i++){
 		if(strb[i] !== strb[len -i-1]){
